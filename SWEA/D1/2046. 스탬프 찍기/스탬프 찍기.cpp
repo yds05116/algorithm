5bd5ad596330a72938c073d02d2f89316ad9c1ff
Lee Seung Hwan <yds05116@naver.com>
2023-11-15 12:18:30 +0900

    [D1] Title: 스탬프 찍기, Time: 5 ms, Memory: 13,540 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "#";
+    }
+    return 0;
+}
