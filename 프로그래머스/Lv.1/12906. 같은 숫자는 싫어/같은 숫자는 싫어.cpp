0e8a3d8f41adebca0fafbcd175a2fb0fa16b0ea5
Lee Seung Hwan <yds05116@naver.com>
2024-01-12 13:07:24 +0900

    [level Lv.1] Title: 같은 숫자는 싫어, Time: 119.76 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    for(int i=0; i<arr.size(); i++){
+        if(answer.size() == 0 || answer.back() != arr[i])
+        answer.push_back(arr[i]);
+    }
+    return answer;
+}
